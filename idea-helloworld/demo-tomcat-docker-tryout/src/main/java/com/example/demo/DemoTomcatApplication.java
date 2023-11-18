7451cfac51004b71d718ac15519db77f74aef8df
Louis Law <louiscklaw@users.noreply.github.com>
2023-11-18 22:45:23 +0800

    update,

--- idea-helloworld/demo-tomcat-docker-tryout/src/main/java/com/example/demo/DemoTomcatApplication.java
+++ idea-helloworld/demo-tomcat-docker-tryout/src/main/java/com/example/demo/DemoTomcatApplication.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoTomcatApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoTomcatApplication.class, args);
 	}
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
